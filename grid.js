bd397c5379632eb30fcc61f471d73827d6ae8071
Viktor Gazsi <Beltorion@users.noreply.github.com>
2023-10-18 16:05:57 +0200

    grid for 4 and 6 elements implemented

--- grid.js
+++ grid.js
@@ -1,6 +1,43 @@
 let collectionGrid = document.querySelector('.category-collection-list-w')
 let collectionGridItem = collectionGrid.querySelectorAll('.category-collection-item')
 
-collectionGridItem.forEach((item, index) => {
- item.classList.add(`item${index + 1}`)
-})
+function arangeGrid() {
+ if (collectionGridItem.length === 6) {
+  elements6()
+ }
+ if (collectionGridItem.length === 4) {
+  elements4()
+ }
+}
+
+function elements6() {
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   item.style.setProperty('grid-column', 'span 3', 'important')
+   item.style.setProperty('aspect-ratio', '3 / 2', 'important')
+  }
+  if (index === 2) {
+   item.style.setProperty('grid-column', 'span 6', 'important')
+   item.style.setProperty('aspect-ratio', '3 / 1', 'important')
+  }
+  if (index === 3 || index === 4 || index === 5) {
+   item.style.setProperty('grid-column', 'span 2', 'important')
+   item.style.setProperty('aspect-ratio', '1 / 1', 'important')
+  }
+ })
+}
+
+function elements4() {
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1 || index === 2) {
+   item.style.setProperty('grid-column', 'span 2', 'important')
+   item.style.setProperty('aspect-ratio', '1 / 1', 'important')
+  }
+  if (index === 3) {
+   item.style.setProperty('grid-column', 'span 6', 'important')
+   item.style.setProperty('aspect-ratio', '3 / 1', 'important')
+  }
+ })
+}
+
+arangeGrid()
