602af1ad0fa20a16bdb9e18e21345bf27b5da238
Viktor Gazsi <Beltorion@users.noreply.github.com>
2023-10-18 22:31:08 +0200

    setting collumns has been extracted to  functions

--- grid.js
+++ grid.js
@@ -1,169 +1,156 @@
-let collectionGrid = document.querySelector('.category-collection-list-w');
-let collectionGridItem = collectionGrid.querySelectorAll(
-  '.category-collection-item'
-);
+let collectionGrid = document.querySelector('.category-collection-list-w')
+let collectionGridItem = collectionGrid.querySelectorAll('.category-collection-item')
 
 function arangeGrid() {
-  if (collectionGridItem.length === 3) {
-    elements3();
-  }
-  if (collectionGridItem.length === 4) {
-    elements4();
-  }
-  if (collectionGridItem.length === 5) {
-    elements5();
-  }
-  if (collectionGridItem.length === 6) {
-    elements6();
-  }
-  if (collectionGridItem.length === 7) {
-    elements7();
-  }
-  if (collectionGridItem.length === 8) {
-    elements8();
-  }
-  if (collectionGridItem.length === 9) {
-    elements9();
-  }
-  if (collectionGridItem.length === 10) {
-    elements10();
-  }
+ if (collectionGridItem.length === 3) {
+  elements3()
+ }
+ if (collectionGridItem.length === 4) {
+  elements4()
+ }
+ if (collectionGridItem.length === 5) {
+  elements5()
+ }
+ if (collectionGridItem.length === 6) {
+  elements6()
+ }
+ if (collectionGridItem.length === 7) {
+  elements7()
+ }
+ if (collectionGridItem.length === 8) {
+  elements8()
+ }
+ if (collectionGridItem.length === 9) {
+  elements9()
+ }
+ if (collectionGridItem.length === 10) {
+  elements10()
+ }
 }
 
 function elements3() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2) {
-      item.style.setProperty('grid-column', 'span 6', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 1', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2) {
+   oneCollumn(item)
+  }
+ })
 }
 
 function elements4() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1 || index === 2) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 3) {
-      item.style.setProperty('grid-column', 'span 6', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 1', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1 || index === 2) {
+   threeCollumns(item)
+  }
+  if (index === 3) {
+   oneCollumn(item)
+  }
+ })
 }
 
 function elements5() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2 || index === 3 || index === 4) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2 || index === 3 || index === 4) {
+   threeCollumns(item)
+  }
+ })
 }
 
 function elements6() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2) {
-      item.style.setProperty('grid-column', 'span 6', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 1', 'important');
-    }
-    if (index === 3 || index === 4 || index === 5) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2) {
+   oneCollumn(item)
+  }
+  if (index === 3 || index === 4 || index === 5) {
+   threeCollumns(item)
+  }
+ })
 }
 
 function elements7() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2 || index === 3 || index === 4) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 5 || index === 6) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2 || index === 3 || index === 4) {
+   threeCollumns(item)
+  }
+  if (index === 5 || index === 6) {
+   twoCollumns(item)
+  }
+ })
 }
 
 function elements8() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2) {
-      item.style.setProperty('grid-column', 'span 6', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 1', 'important');
-    }
-    if (index === 3 || index === 4 || index === 5) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 6 || index === 7) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2) {
+   oneCollumn(item)
+  }
+  if (index === 3 || index === 4 || index === 5) {
+   threeCollumns(item)
+  }
+  if (index === 6 || index === 7) {
+   twoCollumns(item)
+  }
+ })
 }
 
 function elements9() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1 || index === 2) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 3) {
-      item.style.setProperty('grid-column', 'span 6', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 1', 'important');
-    }
-    if (index === 4 || index === 5 || index === 6) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 7 || index === 8) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1 || index === 2) {
+   threeCollumns(item)
+  }
+  if (index === 3) {
+   oneCollumn(item)
+  }
+  if (index === 4 || index === 5 || index === 6) {
+   threeCollumns(item)
+  }
+  if (index === 7 || index === 8) {
+   twoCollumns(item)
+  }
+ })
 }
 
 function elements10() {
-  collectionGridItem.forEach((item, index) => {
-    if (index === 0 || index === 1) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 2 || index === 3 || index === 4) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-    if (index === 5 || index === 6) {
-      item.style.setProperty('grid-column', 'span 3', 'important');
-      item.style.setProperty('aspect-ratio', '3 / 2', 'important');
-    }
-    if (index === 7 || index === 8 || index === 9) {
-      item.style.setProperty('grid-column', 'span 2', 'important');
-      item.style.setProperty('aspect-ratio', '1 / 1', 'important');
-    }
-  });
+ collectionGridItem.forEach((item, index) => {
+  if (index === 0 || index === 1) {
+   twoCollumns(item)
+  }
+  if (index === 2 || index === 3 || index === 4) {
+   threeCollumns(item)
+  }
+  if (index === 5 || index === 6) {
+   twoCollumns(item)
+  }
+  if (index === 7 || index === 8 || index === 9) {
+   threeCollumns(item)
+  }
+ })
+}
+function oneCollumn(item) {
+ item.style.setProperty('grid-column', 'span 6', 'important')
+ item.style.setProperty('aspect-ratio', '3 / 1', 'important')
 }
 
-arangeGrid();
+function twoCollumns(item) {
+ item.style.setProperty('grid-column', 'span 3', 'important')
+ item.style.setProperty('aspect-ratio', '3 / 2', 'important')
+}
+function threeCollumns(item) {
+ item.style.setProperty('grid-column', 'span 2', 'important')
+ item.style.setProperty('aspect-ratio', '1 / 1', 'important')
+}
+
+arangeGrid()
