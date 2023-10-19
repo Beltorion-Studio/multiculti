cf0440c7c2c79ac1bb6535d9527c958dce55753b
Viktor Gazsi <Beltorion@users.noreply.github.com>
2023-10-19 14:14:39 +0200

    testing

--- grid.js
+++ grid.js
@@ -21,10 +21,10 @@ function arrangeGrid() {
 
 function elements3() {
  collectionGridItem.forEach((item, index) => {
-  if (index === 0 || index === 1) {
+  if (index === 1 || index === 2) {
    twoCollumns(item)
   }
-  if (index === 2) {
+  if (index === 0) {
    oneCollumn(item)
   }
  })
