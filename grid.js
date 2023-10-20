7362f34ac34dce36341b1e33cf4a9f77fd87ffa5
Viktor Gazsi <Beltorion@users.noreply.github.com>
2023-10-20 22:07:34 +0200

    grid changed

--- grid.js
+++ grid.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
  // Initial execution on page load
  if (window.innerWidth > 768) {
-  arrangeGrid()
  }
 
  // window.addEventListener('resize', handleResize)
@@ -23,7 +22,7 @@ function removeGridArrangement() {
   item.style.setProperty('aspect-ratio', '3 / 2', 'important')
  })
 }
-
+arrangeGrid()
 function arrangeGrid() {
  const gridFunctions = {
   3: elements3,
